f09f4e198b5ad4830df8459ec8a8cf611cb0f573
Chaoran Lu <andyluloveusc@gmail.com>
2024-12-26 16:54:39 -0800

    delete some png file that never used and test the current scss

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,13 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Hello World!</h1>
+        <h2>YMCA</h2>
+        <h3>Typescript YYDS</h3>
+        <hr/>
+        <code>
+          const fibonacci = fibo(n - 1) + fibo(n - 2);
+        </code>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
