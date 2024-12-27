f8c73e1aeacf9ad2773c3340106ecb445abb0129
Chaoran Lu <andyluloveusc@gmail.com>
2024-12-26 22:16:19 -0800

    add style to the button component

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,12 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <Button disabled> Test </Button>
-        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}> Primary Test </Button>
+        <Button> Test </Button>
+        <Button disabled> Test Disabled</Button>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}> Large Primary </Button>
+        <Button btnType={ButtonType.Danger} size={ButtonSize.Small}> Small Danger </Button>
         <Button btnType={ButtonType.Link} href="http://www.extron.com"> Link Test </Button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button btnType={ButtonType.Link} href="http://www.extron.com" disabled> Disabled Link Test </Button>
       </header>
     </div>
   );
