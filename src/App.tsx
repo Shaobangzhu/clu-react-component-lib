9d88ea7aa43c9ba10491d9e6d8b205dd9d128082
Chaoran Lu <andyluloveusc@gmail.com>
2024-12-26 22:28:59 -0800

    add attribute to Button component

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,11 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <Button> Test </Button>
+        <Button onClick={(e) => {e.preventDefault(); alert("This is a test.")}}> Test </Button>
         <Button disabled> Test Disabled</Button>
         <Button btnType={ButtonType.Primary} size={ButtonSize.Large}> Large Primary </Button>
         <Button btnType={ButtonType.Danger} size={ButtonSize.Small}> Small Danger </Button>
-        <Button btnType={ButtonType.Link} href="http://www.extron.com"> Link Test </Button>
+        <Button btnType={ButtonType.Link} href="http://www.extron.com" target="_blank"> Link Test </Button>
         <Button btnType={ButtonType.Link} href="http://www.extron.com" disabled> Disabled Link Test </Button>
       </header>
     </div>
